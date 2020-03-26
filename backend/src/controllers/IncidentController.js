7ec1b6ba397c2b109468ca15bb0ced4a93f36eb7
Guilherme <gbletsch@gmail.com>
2020-03-26 17:09:04 -0300

    backend done

---
+++ backend/src/controllers/IncidentController.js
@@ -0,0 +1,59 @@
+const connection = require('../database/connection')
+
+module.exports = {
+    async index (request, response) {
+        const { page = 1 } = request.query
+        const incidentsPerPage = 5
+
+        const [count] = await connection('incidents').count()
+        response.header('X-Total-Count', count['count(*)'])
+
+        const incidents = await connection('incidents')
+            .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+            .limit(incidentsPerPage)
+            .offset((page - 1) * incidentsPerPage)
+            .select([
+                'incidents.*',
+                'ongs.name',
+                'ongs.email',
+                'ongs.whatsapp',
+                'ongs.city',
+                'ongs.uf'
+            ])
+        return response.json(incidents)
+    },
+
+    async create (request, response) {
+        console.log('aqui')
+        const { title, description, value } = request.body
+        const ong_id = request.headers.authorization
+
+        const [id] = await connection('incidents').insert({
+            title,
+            description,
+            value,
+            ong_id
+        })
+        return response.json({ id })
+    },
+
+    async delete (request, response) {
+        const { id } = request.params
+        const ong_id = request.headers.authorization
+
+        const incident = await connection('incidents')
+            .where('id', id)
+            .select('ong_id')
+            .first()
+        
+        if (incident.ong_id !== ong_id) {
+            return response.status(401).json({ error: 'Operation not permitted.' })
+        }
+
+        await connection('incidents')
+            .where('id', id)
+            .delete()
+
+        return response.status(204).send()
+    }
+}
---
+++ backend/src/controllers/ProfileController.js
@@ -0,0 +1,19 @@
+const connection = require('../database/connection')
+
+module.exports = {
+    async index (request, response) {
+        const ong_id = request.headers.authorization
+
+        try {
+            const incidents = await  connection('incidents')
+                .where('ong_id', ong_id)
+                .select('*')
+            return response.json(incidents)
+        } catch (error) {
+            return response
+                .status(404)
+                .json({ error })            
+        }
+       
+    }
+}
