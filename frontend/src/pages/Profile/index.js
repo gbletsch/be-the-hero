13714a8abdc034d2a1f17ba0f07a142dc45f9517
Guilherme <gbletsch@gmail.com>
2020-03-27 13:06:23 -0300

    profile page

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import logoImg from '../../assets/logo.svg'
 import { Link } from 'react-router-dom'
-import { FiPower } from 'react-icons/fi'
+import { FiPower, FiTrash2 } from 'react-icons/fi'
 import './styles.css'
 
 export default function Profile () {
@@ -16,6 +16,110 @@ export default function Profile () {
           <FiPower size={18} color='#e02041' />
         </button>
       </header>
+
+      <h1>Casos cadastrados</h1>
+
+      <ul>
+        <li>
+          <strong>CASO:</strong>
+          <p>Caso teste 1</p>
+
+          <strong>DESCRIÇÃO:</strong>
+          <p>Descrição teste</p>
+
+          <strong>VALOR:</strong>
+          <p>R$ 120,00</p>
+
+          <button type='button'>
+            <FiTrash2 size={20} color='#a8a8b3' />
+          </button>
+        </li>
+        <li>
+          <strong>CASO:</strong>
+          <p>Caso teste 1</p>
+
+          <strong>DESCRIÇÃO:</strong>
+          <p>Descrição teste</p>
+
+          <strong>VALOR:</strong>
+          <p>R$ 120,00</p>
+
+          <button type='button'>
+            <FiTrash2 size={20} color='#a8a8b3' />
+          </button>
+        </li>
+        <li>
+          <strong>CASO:</strong>
+          <p>Caso teste 1</p>
+
+          <strong>DESCRIÇÃO:</strong>
+          <p>Descrição teste</p>
+
+          <strong>VALOR:</strong>
+          <p>R$ 120,00</p>
+
+          <button type='button'>
+            <FiTrash2 size={20} color='#a8a8b3' />
+          </button>
+        </li>
+        <li>
+          <strong>CASO:</strong>
+          <p>Caso teste 1</p>
+
+          <strong>DESCRIÇÃO:</strong>
+          <p>Descrição teste</p>
+
+          <strong>VALOR:</strong>
+          <p>R$ 120,00</p>
+
+          <button type='button'>
+            <FiTrash2 size={20} color='#a8a8b3' />
+          </button>
+        </li>
+        <li>
+          <strong>CASO:</strong>
+          <p>Caso teste 1</p>
+
+          <strong>DESCRIÇÃO:</strong>
+          <p>Descrição teste</p>
+
+          <strong>VALOR:</strong>
+          <p>R$ 120,00</p>
+
+          <button type='button'>
+            <FiTrash2 size={20} color='#a8a8b3' />
+          </button>
+        </li>
+        <li>
+          <strong>CASO:</strong>
+          <p>Caso teste 1</p>
+
+          <strong>DESCRIÇÃO:</strong>
+          <p>Descrição teste</p>
+
+          <strong>VALOR:</strong>
+          <p>R$ 120,00</p>
+
+          <button type='button'>
+            <FiTrash2 size={20} color='#a8a8b3' />
+          </button>
+        </li>
+        <li>
+          <strong>CASO:</strong>
+          <p>Caso teste 1</p>
+
+          <strong>DESCRIÇÃO:</strong>
+          <p>Descrição teste</p>
+
+          <strong>VALOR:</strong>
+          <p>R$ 120,00</p>
+
+          <button type='button'>
+            <FiTrash2 size={20} color='#a8a8b3' />
+          </button>
+        </li>
+        
+      </ul>
     </div>
   )
 }
