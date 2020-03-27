5cab3b164b39830089890e045347f1383fb1d75d
Guilherme <gbletsch@gmail.com>
2020-03-27 16:50:53 -0300

    connect login to api

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,23 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FiLogIn } from 'react-icons/fi'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import './styles.css'
 import '../../global.css'
+import api from '../../services/api'
 
 import heroesImg from '../../assets/heroes.png'
 import logoImg from '../../assets/logo.svg'
 
 export default function Logon () {
+  const [id, setId] = useState('')
+  const history = useHistory()
+
+  async function handleLogin (e) {
+    e.preventDefault()
+    console.log('aqui')
+    try {
+      const response = await api.post('sessions', { id })
+      window.localStorage.setItem('ongId', id)
+      window.localStorage.setItem('ongName', response.data.name)
+      history.push('/profile')
+    } catch (error) {
+      window.alert('ID inexistente, tente novamente')
+    }
+  }
+
   return (
     <div className='logon-container'>
       <section className='form'>
 
         <img src={logoImg} alt='Be The Hero' />
 
-        <form>
+        <form onSubmit={handleLogin}>
           <h1>Faça seu logon</h1>
 
-          <input type='text' placeholder='Sua ID' />
+          <input
+            type='text'
+            placeholder='Sua ID'
+            value={id}
+            onChange={e => setId(e.target.value)}
+          />
           <button className='button' type='submit'>Entrar</button>
           <Link className='back-link' to='/register'>
             <FiLogIn size={16} color='#E02041' />
