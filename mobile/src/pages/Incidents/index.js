2097c16bc5a0aed20602fd14754c47e6bf62a08c
Guilherme <gbletsch@gmail.com>
2020-03-28 09:13:35 -0300

    splash and logo instal react-navigation

---
+++ mobile/src/pages/Incidents/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Incidents = () => {
+  return (
+    <View>
+      <Text>Hello, Incidents</Text>
+    </View>
+  )
+}
+
+export default Incidents
