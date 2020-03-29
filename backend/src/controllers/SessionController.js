1fa89980a4fba36eafd199c65f0260f6a7a8b460
Guilherme <gbletsch@gmail.com>
2020-03-29 06:48:34 -0300

    validation tests

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -1,18 +1,18 @@
 const connection = require('../database/connection')
 
 module.exports = {
-    async create (request, response) {
-        const { id } = request.body
+  async create (request, response) {
+    const { id } = request.body
 
-        const ong = await connection('ongs')
-            .where('id', id)
-            .select('name')
-            .first()
-        if (!ong) {
-            return response
-                .status(400)
-                .json({ error: 'wrong ID' })
-        }
-        return response.json(ong)
+    const ong = await connection('ongs')
+      .where('id', id)
+      .select('name')
+      .first()
+    if (!ong) {
+      return response
+        .status(400)
+        .json({ error: 'wrong ID' })
     }
+    return response.json(ong)
+  }
 }
