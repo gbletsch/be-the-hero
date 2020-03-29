e5c33ec8f073d9240dc4f5f4915fdf67940fb129
Guilherme <gbletsch@gmail.com>
2020-03-29 09:08:54 -0300

    tests

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const routes = require('./routes')
 const cors = require('cors')
 const { errors } = require('celebrate')
- 
+
 const app = express()
 
 app.use(cors())
@@ -10,4 +10,4 @@ app.use(express.json())
 app.use(routes)
 app.use(errors())
 
-app.listen(3333)
+module.exports = app
